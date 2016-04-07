6a87aa1fe9f0c55f64a9a17b85af90937d17bcc4
tmp <jhg@gkjigi>
2016-04-07 11:54:31 +0300

    Работа дома

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 /**
  * Created by tmp on 07.04.2016.
  */
-public class Hello {
+public class  Hello {
     public static void main(String[] args) {
         System.out.println("jjj");
     }
---
+++ src/Hello2.java
@@ -0,0 +1,5 @@
+/**
+ * Created by tmp on 07.04.2016.
+ */
+public class Hello2 {
+}
